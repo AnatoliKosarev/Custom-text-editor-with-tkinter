c6d74bd878992de336eaa8492fe13eee702d1226
anatoli.kosarev <anatoli.kosarev@gmail.com>
2021-07-28 22:34:17 +0300

    notebook functionality, menu commands added

--- Notebook_project/app.py
+++ Notebook_project/app.py
@@ -1,13 +1,90 @@
 import os
 import tkinter as tk
-from tkinter import ttk, filedialog
+from tkinter import ttk, filedialog, messagebox
 
 
-def create_file():
-    text_area = tk.Text(notebook)
-    text_area.pack(fill='both', expand=True)
-    notebook.add(text_area, text='Untitled')
-    notebook.select(text_area)
+text_contents = dict()
+
+
+def create_file(content='', title='Untitled'):
+    container = ttk.Frame(notebook)
+    container.pack()
+
+    text_area = tk.Text(container)
+    text_area.pack(side='left', fill='both', expand=True)
+    text_area.insert('end', content)
+    notebook.add(container, text=title)
+    notebook.select(container)
+
+    text_contents[str(text_area)] = hash(content)
+
+    text_scroll = ttk.Scrollbar(container, orient='vertical', command=text_area.yview)
+    text_scroll.pack(side='right', fill='y')
+    text_area['yscrollcommand'] = text_scroll.set
+
+
+def check_for_changes():
+    current = get_text_widget()
+    content = current.get('1.0', 'end-1c')
+    name = notebook.tab('current')['text']
+
+    if hash(content) != text_contents[str(current)]:
+        if name[-1] != '*':
+            notebook.tab('current', text=name + '*')
+    elif name[-1] == '*':  # if the text is the same and there is * in tab name - remove it
+        notebook.tab('current', text=name[:-1])
+
+
+def get_current_tab():
+    return notebook.nametowidget(notebook.select())
+
+
+def get_text_widget():
+    tab_widget = get_current_tab()
+    text_widget = tab_widget.winfo_children()[0]
+    return text_widget
+
+
+def close_current_tab():
+    current = get_current_tab()
+    if current_tab_unsaved() and not confirm_close():
+        return
+    if len(notebook.tabs()) == 1:
+        create_file()
+
+    notebook.forget(current)
+
+
+def current_tab_unsaved():
+    text_widget = get_text_widget()
+    content = text_widget.get('1.0', 'end-1c')
+    return hash(content) != text_contents[str(text_widget)]
+
+
+def confirm_close():
+    return messagebox.askyesno(
+            message='You have unsaved changes. Are you sure you want to close?',
+            icon='question',
+            title='Unsaved Changes'
+        )
+
+
+def confirm_quit():
+    unsaved = False
+
+    for tab in notebook.tabs():
+        tab_widget = root.nametowidget(tab)
+        text_widget = tab_widget.winfo_children()[0]
+        content = text_widget.get('1.0', 'end-1c')
+
+        if hash(content) != text_contents[str(text_widget)]:
+            unsaved = True
+            break
+
+    if unsaved and not confirm_close():
+        return
+
+    root.destroy()
 
 
 def save_file():
@@ -15,7 +92,7 @@ def save_file():
 
     try:
         filename = os.path.basename(file_path)
-        text_widget = root.nametowidget(notebook.select())
+        text_widget = get_text_widget()
         content = text_widget.get('1.0', 'end-1c')
 
         with open(file_path, 'w') as file:
@@ -26,6 +103,30 @@ def save_file():
         return
 
     notebook.tab('current', text=filename)
+    text_contents[str(text_widget)] = hash(content)
+
+
+def open_file():
+    file_path = filedialog.askopenfilename()
+
+    try:
+        filename = os.path.basename(file_path)
+
+        with open(file_path, 'r') as file:
+            content = file.read()
+
+    except (AttributeError, FileNotFoundError):
+        print('Open operation cancelled')
+        return
+
+    create_file(content, filename)
+
+
+def show_about():
+    messagebox.showinfo(
+        title='About',
+        message='Custom editor is a simple tabbed text editor designed to help learn Tkinter.'
+    )
 
 
 root = tk.Tk()
@@ -39,16 +140,28 @@ menubar = tk.Menu()
 root.config(menu=menubar)
 
 file_menu = tk.Menu(menubar)
-menubar.add_cascade(menu=file_menu, label='File')
+help_menu = tk.Menu(menubar)
 
-file_menu.add_command(label='New', command=create_file)
-file_menu.add_command(label='Save', command=save_file)
+menubar.add_cascade(menu=file_menu, label='File')
+menubar.add_cascade(menu=help_menu, label='Help')
+
+file_menu.add_command(label='New', command=create_file, accelerator='Ctrl+N')
+file_menu.add_command(label='Open', command=open_file, accelerator='Ctrl+O')
+file_menu.add_command(label='Save', command=save_file, accelerator='Ctrl+S')
+file_menu.add_command(label="Close Tab", command=close_current_tab, accelerator='Ctrl+Q')
+file_menu.add_command(label='Exit', command=confirm_quit)
+
+help_menu.add_command(label='About', command=show_about)
 
 notebook = ttk.Notebook(main)
 notebook.pack(fill='both', expand=True)
+create_file()
 
-create_file()
-create_file()
+root.bind('<KeyPress>', lambda event: check_for_changes())
+root.bind('<Control-n>', lambda event: create_file())
+root.bind('<Control-o>', lambda event: open_file())
+root.bind('<Control-s>', lambda event: save_file())
+root.bind('<Control-q>', lambda event: close_current_tab())
 
 root.mainloop()
 
